98e261d7da61c51817c4807befc4139bc7fd8e8c
seecretsaaanta <seecretsaaanta@gmail.com>
2020-12-11 15:27:11 +0530

    More changes

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ var flyArea = $("#flyarea").height();
 var score = 0;
 var highscore = 0;
 
-// var pipeheight = 90;
-var pipeheight = 300;
+var pipeheight = 90;
+// var pipeheight = 300;
 var pipewidth = 52;
 var pipes = new Array();
 
